b28281a742deb2bf0865501c5106a1397747eb85
yaoshengmarketing-prog <yaosheng.marketing@gmail.com>
2026-07-17 21:04:58 +0800

    Add files via upload

---
+++ src/pages/sitemap.xml.js
@@ -0,0 +1,34 @@
+import { getCollection } from 'astro:content';
+import { CATEGORIES, SITE } from '../consts';
+
+export async function GET() {
+  const posts = (await getCollection('blog')).filter((p) => !p.data.draft);
+  const iso = (d) => new Date(d).toISOString();
+  const now = new Date().toISOString();
+
+  const staticPages = [
+    ['/deposit-guide/', '0.7'], ['/withdraw-guide/', '0.7'],
+    ['/security-license/', '0.6'], ['/terms/', '0.3'], ['/privacy/', '0.3'],
+  ];
+
+  const urls = [
+    { loc: `${SITE.url}/`, lastmod: now, priority: '1.0' },
+    { loc: `${SITE.url}/blog/`, lastmod: now, priority: '0.8' },
+    ...Object.keys(CATEGORIES).map((c) => ({
+      loc: `${SITE.url}/${c}/`, lastmod: now, priority: '0.6',
+    })),
+    ...staticPages.map(([loc, priority]) => ({ loc: `${SITE.url}${loc}`, lastmod: now, priority })),
+    ...posts.map((p) => ({
+      loc: `${SITE.url}/blog/${p.slug}/`,
+      lastmod: iso(p.data.updatedDate ?? p.data.pubDate),
+      priority: '0.7',
+    })),
+  ];
+
+  const body = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls.map((u) => `  <url><loc>${u.loc}</loc><lastmod>${u.lastmod}</lastmod><priority>${u.priority}</priority></url>`).join('\n')}
+</urlset>`;
+
+  return new Response(body, { headers: { 'Content-Type': 'application/xml' } });
+}
